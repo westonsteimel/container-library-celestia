04073f1c4e2886db01cdb274b2855c38fb8f4879
Weston Steimel <weston.steimel@gmail.com>
2019-08-22 23:53:25 +0100

    remove old tagging script
    Signed-off-by: Weston Steimel <weston.steimel@gmail.com>

--- tag.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-VERSION=$(grep -e "ARG CELESTIA_VERSION=" Dockerfile)
-VERSION=${VERSION#ARG CELESTIA_VERSION=\"}
-VERSION=${VERSION%\"}
-echo "Tagging version ${VERSION}"
-docker tag "${DOCKER_USERNAME}/celestia:latest" "${DOCKER_USERNAME}/celestia:${VERSION}"
-docker tag "${DOCKER_USERNAME}/celestia:latest" "${DOCKER_USERNAME}/celestia:master"
